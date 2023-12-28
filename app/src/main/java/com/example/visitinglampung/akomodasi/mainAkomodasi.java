2a61ccb366be5675d88109b53b1061f872433682
BeDhoel <nathannurdadyansyah92@gmail.com>
2023-12-28 13:44:58 +0700

    Add akomodasi layout

---
+++ app/src/main/java/com/example/visitinglampung/akomodasi/mainAkomodasi.java
@@ -0,0 +1,16 @@
+package com.example.visitinglampung.akomodasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.visitinglampung.R;
+
+public class mainAkomodasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_akomodasi);
+    }
+}
