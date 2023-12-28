ebfaee20e5299e24ee74514ad94e6d9586bc53d5
BeDhoel <nathannurdadyansyah92@gmail.com>
2023-12-28 10:59:45 +0700

    Fixing bug and add paket, informasi layout

---
+++ app/src/main/java/com/example/visitinglampung/informasi/mainInformasi.java
@@ -0,0 +1,16 @@
+package com.example.visitinglampung.informasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.visitinglampung.R;
+
+public class mainInformasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_informasi);
+    }
+}
---
+++ app/src/main/java/com/example/visitinglampung/paket_wisata/mainPaket.java
@@ -0,0 +1,16 @@
+package com.example.visitinglampung.paket_wisata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.visitinglampung.R;
+
+public class mainPaket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_paket);
+    }
+}
